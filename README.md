119ef7cddfbaf270e309bd600e55a9ee6bd229f0
somekindofpanem <panemlp@gmx.net>
2021-01-15 00:27:18 +0100

    fet: added bootloader

--- README.md
+++ README.md
@@ -15,3 +15,12 @@ Filetypes:
 Starter applications:
     - **Catapult**: This program allows you to launch any .exe from windows on your cletyxo machine.
     - **Notepad**: A basic text editor
+
+
+## How to get started
+[More comming soon :)]
+Install it, setup the kernel by going into kernel and using the "make setup" command, build the gnu-efi by going into it and doing "make setup"
+Compile the kernel using "make kernel" and then "make buildimg" to boot it.
+You can also just use "make pfb"
+[Linux only]
+Use "make boot" in kernel to compile, link, make the buildimg and then boot it using qemu.
--- gnu-efi/bootloader/main.c
+++ gnu-efi/bootloader/main.c
@@ -1,22 +1,75 @@
 #include <efi.h>
 #include <efilib.h>
+#include <elf.h>
+
+
+EFI_FILE* LoadFile(EFI_FILE* Directory, CHAR16* Path, EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE* SystemTable) 
+{
+	EFI_FILE* LoadedFile;
+	EFI_LOADED_IMAGE_PROTOCOL* LoadedImage;
+	SystemTable->BootServices->HandleProtocol(ImageHandle, &gEfiLoadedImageProtocolGuid, (void**)&LoadedImage);
+
+	EFI_SIMPLE_FILE_SYSTEM_PROTOCOL* FileSystem;
+	SystemTable->BootServices->HandleProtocol(LoadedImage->DeviceHandle, &gEfiSimpleFileSystemProtocolGuid, (void**)&FileSystem);
+
+	if (Directory == NULL)
+	{
+		FileSystem->OpenVolume(FileSystem, &Directory);
+	}
+
+	EFI_STATUS s = Directory->Open(Directory, &LoadedFile, Path, EFI_FILE_MODE_READ, EFI_FILE_READ_ONLY);
+	if (s != EFI_SUCCESS)
+	{
+		return NULL;
+	} 
+	return LoadedFile;
+
+}
+
+
+int memcmp(const void* aptr, const void* bptr, size_T n)
+{
+	const unsigned char* a = aptr, b = bptr;
+	for (size_t i = 0; i < n; i++)
+	{
+		if (a[i] < b[i]) return -1;
+		else if (a[i] > b[i]) return 1;
+	}
+	return 0;
+}
+
+
 
 EFI_STATUS efi_main (EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable) {
-	EFI_STATUS Status;
-	EFI_INPUT_KEY Key;
+	InitializeLib(ImageHandle, SystemTable);
 
-	ST = SystemTable; //Store the system table
+	Print(L"Visionizer Cletyxo [ Version 0.1 Pre-Alpha ] \n\r"); // Get version automatically
+	Print(L"(c) 2021 Visionizer Corporation. All rights reserverd \n\r");
 
-	Status = ST->ConOut->OutputString(ST->ConOut, L"HelloWorld!\n\r"); // Print HelloWorld to the console
+	EFI_FILE* Kernel = LoadFile(NULL, L"kernel.elf", ImageHandle, SystemTable);
+	if(Kernel == NULL) 
+	{
+		Print(L"[Bootloader] Fatal: Could not load kernel.elf. Please restart your computer.\n\r"); // Better msg TODO
+	} // Maybe add checkmarks TODO
+	else {
+		Print(L"[Bootloader] Success: Kernel loaded succesfully \n\r");
+	}
 
-	if (EFI_ERROR(Status)) //If printing failed, return
-		return Status;
+	Elf64_Ehdr header;
+	{
+		UINTN FileInfoSize;
+		EFI_FILE_INFO* FIleInfo;
+		Kernel->GetInfo(Kernel, &gEfiFileInfoGuid, &FileInfoSize, NULL);
+		SystemTable->BootServices->AllocatePool(EfiLoaderData, FileInfoSize, (void**)&FileInfo);
+		Kernel->GetInfo(Kernel, &gEfiFileInfoGuid, &FileInfoSize, (void**)&FileInfo);
 
-	Status = ST->ConIn->Reset(ST->ConIn, FALSE); // Empty the Console Input Buffer
-	if (EFI_ERROR(Status))
-		return Status;
+		UINTN size = sizeof(header);
+		Kernel->Read(Kernel, &size, &header);
 
-	while ((Status = ST->ConIn->ReadKeyStroke(ST->ConIn, &Key)) == EFI_NOT_READY) ; // Poll For Key Input
+	} if(
+		)
 
-	return Status; // Exit the UEFI application
+
+
+	return EFI_SUCCESS; // Exit the UEFI application
 }
